c3bf59b712c6ea0ef479cb2256fa8441fdab690e
Maria <maam@wp.pl>
2023-11-23 22:08:40 +0100

    Dodany nowy plik

---
+++ dla_Ola.py
@@ -0,0 +1,10 @@
+while True:
+    odp=input('Napisz, czy lubisz pingwiny (tak lub nie)')
+
+    if odp.lower()=='tak':
+        print('Jestes fajny!')
+        break
+    elif odp.lower()=='nie':
+        print('Nie lubie Cie!')
+    else:
+        print('Nie rozumiem')
